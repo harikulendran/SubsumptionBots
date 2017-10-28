463352110b6e80d604f887d10a489e9bbd2cb018
Hari <hk1n15@ecs.soton.ac.uk>
2017-10-28 14:02:06 +0100

    init

--- SubsumptionBots/SubsumptionBots.cpp
+++ SubsumptionBots/SubsumptionBots.cpp
@@ -1,11 +1,27 @@
 // SubsumptionBots.cpp : Defines the entry point for the console application.
 //
 
-#include "stdafx.h"
+#include <stdio.h>
+#include <allegro5\allegro.h>
 
+int main(int argc, char **argv) {
+	ALLEGRO_DISPLAY *display = NULL;
 
-int main()
-{
-    return 0;
+	if (!al_init()) {
+		fprintf(stderr, "failed to initialize allegro!\n");
+		return 1;
+	}
+
+	display = al_create_display(640, 470);
+	if (!display) {
+		fprintf(stderr, "failed to create display!\n");
+		return 1;
+	}
+
+	al_clear_to_color(al_map_rgb(0, 0, 0));
+	al_flip_display();
+	al_rest(10.0);
+	al_destroy_display(display);
+
+	return 0;
 }
-
