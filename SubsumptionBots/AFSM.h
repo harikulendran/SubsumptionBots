c173eeed099b55b3d2e9d84fb42a0f72666270ac
Hari <hk1n15@ecs.soton.ac.uk>
2017-10-30 00:15:11 +0000

    refactoring AFSM

--- SubsumptionBots/AFSM.h
+++ SubsumptionBots/AFSM.h
@@ -1,18 +1,33 @@
 #pragma once
 #include <map>
+#include <vector>
 #include <string>
 
 struct State {
 	public:
+		bool startState = false;
 		bool finalState = false;
 		int index;
-		State(int i);
+		double value = 0;
+		State(int i, bool ss, bool fs, double val);
+};
+
+struct Transition {
+public:
+	int dest;
+	std::string value;
+	Transition(int d, std::string v);
 };
 
 class AFSM {
 	private:
-		std::map<int, int> transition;
+		std::map<int, Transition> transition;
+		std::vector<State> states;
 
 	public:
+		AFSM();
+		void addState(int i, bool ss, bool fs, double val);
 		void read(std::string i);
 };
+
+//TODO: integrate transitions into states
